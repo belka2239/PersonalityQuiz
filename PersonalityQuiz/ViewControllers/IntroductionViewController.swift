3dfc997846513f123ab75f87d1ac700b5e6839cb
Gurina Elena <belka2239@gmail.com>
2022-03-06 19:08:20 +0300

    Implementation of a button to return to the first screen with clearing windows with questions

---
+++ PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by VG on 03.03.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by VG on 04.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var animalTypeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+//        updateResultVC()
+    }
+    
+//    private func updateResultVC() {
+//        let animals: [Animal] = []
+//
+//        for animal in answers {
+//            if let animalTypeCount = answers(animal) {
+//                animalTypeCount.append(animals)
+//            }
+//        }
+//
+//    }
+    
+/*
+    private func updateResultVC() {
+        var frequentAnimals: [Animal: Int] = [:]
+        let animals = answers.map{ $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = frequentAnimals[animal] {
+                frequentAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequentAnimals[animal] = 1
+            }
+        }
+        
+        let sortedFrequentAnimals = frequentAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequentAnimals = sortedFrequentAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequentAnimals)
+    }
+     */
+    
+//    private func updateUI(with animal: Animal) {
+//        animalTypeLabel.text = "Вы - \(animal.rawValue)!"
+//        descriptionLabel.text = animal.definition
+//    }
+}
